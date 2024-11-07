3073dbcacf4223eab960da74e36a0a873bfe5da2
Deckloins <trilaf12345@gmail.com>
2024-11-06 21:03:10 -0500

    Refactor to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,29 +9,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next(); // ignore first field (program name)
 
-        let mut option_casing = false;    
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };        
 
-        if args.len() > 3 {
-           option_casing = bool::from_str(&args[3]).unwrap();
-        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };        
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        let mut ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        // If the option is set to true but not the ENV activate ignore_case
-        // The other cases like both True or Both False are inhenrently coverded
-        // The cases in which the ENV is set to 0 are not coverded tho
-
-        if option_casing && !ignore_case {
-            ignore_case = true;
-        }
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
@@ -58,15 +51,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
